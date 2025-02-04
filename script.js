35cb4b7ecd21150938c426eff3308d6860128cdf
noah-peter-king <177217902+noah-peter-king@users.noreply.github.com>
2025-02-04 23:23:24 +1100

    Added remove grid and resize grid functions

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 const container = document.querySelector(".container");
+const resize = document.querySelector("#resize")
+resize.addEventListener("click", (event) => {removeGrid() 
+  makeGrid(prompt("Resize The Square"))
+});
 
-for (i = 0; i < 16; i++){
-  let square = document.createElement("div");
-  square.classList.add("square");
-  container.appendChild(square);
-  square.addEventListener("mouseover", (event) => {square.classList.add("hover")});
+makeGrid(100);
 
+function makeGrid(side){
+  for (i = 0; i < side*side; i++){
+    if (side > 100){
+      alert("Less than 100 pretty please") 
+      break;
+    }
+
+    let square = document.createElement("div");    
+    square.classList.add("square");
+    container.appendChild(square);
+    square.addEventListener("mouseover", (event) => {square.classList.add("hover")});
+  }
 }
+function removeGrid(){
+  while (container.hasChildNodes()) {
+    container.removeChild(container.firstChild);
+  }
+}
+
+//square.getElementsByClassName("square").style.height = "960 / 100";
+
 //You could calculate the size of each grid item by getting the width of the container 
 //divided by the number of squares in a row I think
