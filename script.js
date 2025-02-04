db7e337bd247077afaae6d88c630a4e27674893e
noah-peter-king <177217902+noah-peter-king@users.noreply.github.com>
2025-02-04 11:25:38 +1100

    Etch and Sketch now Etches and Sketches

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const container = document.querySelector(".container");
 
 for (i = 0; i < 16; i++){
-  let div = document.createElement("div");
-  div.classList.add("div");
-  container.appendChild(div);
-}
+  let square = document.createElement("div");
+  square.classList.add("square");
+  container.appendChild(square);
+  square.addEventListener("mouseover", (event) => {square.classList.add("hover")});
 
+}
+//You could calculate the size of each grid item by getting the width of the container 
+//divided by the number of squares in a row I think
